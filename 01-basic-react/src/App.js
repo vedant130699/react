e008c2ab031093107269737842cea2cb524f977f
vedant130699 <vedantkulkarni1306@gmail.com>
2025-10-26 11:20:56 +0530

    learned how to install react projects

---
+++ 01-basic-react/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+   <h1>
+    chai aur react | Vedant Kulkarni
+    Today I learned how to install and run react projects using 2 methods.
+    create-react-app utility and vite
+   </h1>
+  );
+}
+
+export default App;
