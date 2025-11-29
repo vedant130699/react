2c3f58abf4cc3a5d5897a5878db3d11f911f1555
vedant130699 <vedantkulkarni1306@gmail.com>
2025-11-29 11:23:48 +0530

    created folder structure for mega-blog-project

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <>
+        <h1>Blog with apprwite</h1>
+      
+    </>
+  )
+}
+
+export default App
